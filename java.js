8124a442015e94ef90a400d53f926e0fd4069f76
Stanislav <cotelan24@gmail.com>
2019-05-22 19:47:36 +0300

    Tab Navigation
    Into Block2 now i have Tab Navigation with some animation with use JS

---
+++ java.js
@@ -0,0 +1,91 @@
+var num1 = false;
+var num2 = false;
+var num3 = false;
+var num4 = false;
+
+
+
+function changeStyle1() {
+    document.getElementById('icon1').style.display = 'none';
+    document.getElementById('icon1_click').style.display = 'flex';
+    
+    document.getElementById('icon2').style.display = 'flex';
+    document.getElementById('icon2_click').style.display = 'none';
+    
+    document.getElementById('icon3').style.display = 'flex';
+    document.getElementById('icon3_click').style.display = 'none';
+    
+    document.getElementById('icon4').style.display = 'flex';
+    document.getElementById('icon4_click').style.display = 'none';
+    
+    
+    document.getElementById('macbookimage').style.display = 'flex';
+    document.getElementById('ipadimage').style.display = 'none';
+    document.getElementById('ipadnewimage').style.display = 'none';
+    document.getElementById('laptopimage').style.display = 'none';
+    
+    
+    }
+
+function changeStyle2() {
+    document.getElementById('icon1').style.display = 'flex';
+    document.getElementById('icon1_click').style.display = 'none';
+    
+    document.getElementById('icon2').style.display = 'none';
+    document.getElementById('icon2_click').style.display = 'flex';
+    
+    document.getElementById('icon3').style.display = 'flex';
+    document.getElementById('icon3_click').style.display = 'none';
+    
+    document.getElementById('icon4').style.display = 'flex';
+    document.getElementById('icon4_click').style.display = 'none';
+    
+    
+    document.getElementById('macbookimage').style.display = 'none';
+    document.getElementById('ipadimage').style.display = 'flex';
+    document.getElementById('ipadnewimage').style.display = 'none';
+    document.getElementById('laptopimage').style.display = 'none';
+    
+    }
+
+function changeStyle3() {
+    document.getElementById('icon1').style.display = 'flex';
+    document.getElementById('icon1_click').style.display = 'none';
+    
+    document.getElementById('icon2').style.display = 'flex';
+    document.getElementById('icon2_click').style.display = 'none';
+    
+    document.getElementById('icon3').style.display = 'none';
+    document.getElementById('icon3_click').style.display = 'flex';
+    
+    document.getElementById('icon4').style.display = 'flex';
+    document.getElementById('icon4_click').style.display = 'none';
+    
+    document.getElementById('macbookimage').style.display = 'none';
+    document.getElementById('ipadimage').style.display = 'none';
+    document.getElementById('ipadnewimage').style.display = 'flex';
+    document.getElementById('laptopimage').style.display = 'none';
+    }
+
+function changeStyle4() {
+    document.getElementById('icon1').style.display = 'flex';
+    document.getElementById('icon1_click').style.display = 'none';
+    
+    document.getElementById('icon2').style.display = 'flex';
+    document.getElementById('icon2_click').style.display = 'none';
+    
+    document.getElementById('icon3').style.display = 'flex';
+    document.getElementById('icon3_click').style.display = 'none';
+    
+    document.getElementById('icon4').style.display = 'none';
+    document.getElementById('icon4_click').style.display = 'flex';
+    
+    document.getElementById('macbookimage').style.display = 'none';
+    document.getElementById('ipadimage').style.display = 'none';
+    document.getElementById('ipadnewimage').style.display = 'none';
+    document.getElementById('laptopimage').style.display = 'flex';
+    
+    }
+
+
+    
